09371a8c69c59b97a669c35f6359868b3df3dbbc
gohy907 <gohy2006@mail.ru>
2025-07-18 17:20:00 +0400

    feat: update container to import git-trainer binary file

--- tasks/task1-container/build.sh
+++ tasks/task1-container/build.sh
@@ -3,7 +3,9 @@
 git_username=$(git config --global user.name)
 git_email=$(git config --global user.email)
 
-git clone git@github.com:git-trainer-tasks/task1 > /dev/null 2>&1
-docker build --build-arg USERNAME=$USER --build-arg GIT_USERNAME=$git_username --build-arg GIT_EMAIL=$git_email --rm -t task1:latest . > /dev/null 2>&1
+git clone git@github.com:git-trainer-tasks/task1
+cp -r ../../git-trainer-in-container .
+docker build --build-arg USERNAME=$USER --build-arg GIT_USERNAME=$git_username --build-arg GIT_EMAIL=$git_email --rm -t task1:latest .
 rm -rf task1
-docker run --rm -it --name task1 task1:latest 
+rm -rf git-trainer-in-container/
+docker run -it --name task1 task1:latest 
