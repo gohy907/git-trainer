ba96787bef7e8337c8ccdd62f0bf83ff8a0b0bd6
gohy907 <gohy2006@mail.ru>
2025-07-20 23:22:47 +0400

    fix: rename tasks

--- tasks/task2/build.sh
+++ tasks/task2/build.sh
@@ -3,8 +3,9 @@
 git_username=$(git config --global user.name)
 git_email=$(git config --global user.email)
 
+git clone git@github.com:git-trainer-tasks/task1
 cp -r ../../git-trainer-in-container .
-docker build --build-arg USERNAME=$USER --build-arg GIT_USERNAME=$git_username --build-arg GIT_EMAIL=$git_email --rm -t task2:latest .
+docker build --build-arg USERNAME=$USER --build-arg GIT_USERNAME=$git_username --build-arg GIT_EMAIL=$git_email --rm -t task1:latest .
 rm -rf task1
 rm -rf git-trainer-in-container/
-docker run -it --name task2 task2:latest 
+docker run -it --name task1 task1:latest 
