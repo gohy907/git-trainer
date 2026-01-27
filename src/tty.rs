e17905edf0b73e5e57fce3cfc9e4b7bd018410ec
gohy907 <gohy2006@mail.ru>
2026-01-27 18:02:37 +0400

    refactor: rewrite key handling and add popup handling

--- src/tty.rs
+++ src/tty.rs
@@ -1,6 +1,6 @@
 use crate::App;
 use crate::task::Task;
-use crate::ui;
+use crate::ui::{self, Popup};
 use bytes::Bytes;
 use crossterm::event::{self, Event, KeyCode, KeyEventKind, KeyModifiers};
 use ratatui::DefaultTerminal;
@@ -56,6 +56,7 @@ pub enum RunPtyError {
 
 impl App {
     pub async fn prepare_pty_bollard(
+        &mut self,
         terminal: &mut DefaultTerminal,
         task: &Task,
     ) -> Result<(), PreparePtyError> {
@@ -137,7 +138,7 @@ impl App {
         });
         handles.push(handle);
 
-        Self::run_pty_bollard(terminal, parser, tx, exit_rx, container_name).await?;
+        Self::run_pty_bollard(self, terminal, parser, tx, exit_rx, container_name).await?;
 
         for handle in handles {
             handle.await.map_err(PreparePtyError::JoinError)?;
@@ -146,6 +147,7 @@ impl App {
     }
 
     pub async fn run_pty_bollard(
+        &mut self,
         terminal: &mut DefaultTerminal,
         parser: Arc<RwLock<vt100::Parser>>,
         sender: Sender<Bytes>,
@@ -163,62 +165,16 @@ impl App {
                 return Ok(());
             }
 
-            terminal.draw(|f| ui::ui_pty(f, parser.read().unwrap().screen()))?;
+            terminal.draw(|f| ui::ui_pty(f, parser.read().unwrap().screen(), self))?;
 
             if event::poll(Duration::from_millis(10))? {
                 match event::read()? {
                     Event::Key(key) => {
                         if key.kind == KeyEventKind::Press {
-                            if key.modifiers == KeyModifiers::CONTROL {
-                                match key.code {
-                                    KeyCode::Char('c') => sender.send(Bytes::from(vec![3]))?,
-                                    KeyCode::Char('v') => sender.send(Bytes::from(vec![22]))?,
-                                    KeyCode::Char('d') => sender.send(Bytes::from(vec![4]))?,
-                                    KeyCode::Char('z') => sender.send(Bytes::from(vec![26]))?,
-                                    KeyCode::Char('l') => sender.send(Bytes::from(vec![12]))?,
-                                    _ => {}
-                                }
+                            if let Some(_) = self.active_popup {
+                                let _ = self.handle_popup_key(key.code);
                             } else {
-                                match key.code {
-                                    KeyCode::Char(input) => {
-                                        sender.send(Bytes::from(input.to_string().into_bytes()))?
-                                    }
-                                    KeyCode::Backspace => {
-                                        sender.send(Bytes::from(vec![8]))?;
-                                    }
-                                    KeyCode::Enter => {
-                                        #[cfg(unix)]
-                                        sender.send(Bytes::from(vec![b'\n']))?;
-                                        #[cfg(windows)]
-                                        sender.send(Bytes::from(vec![b'\r', b'\n']))?;
-                                    }
-                                    KeyCode::Left => sender.send(Bytes::from(vec![27, 91, 68]))?,
-                                    KeyCode::Right => sender.send(Bytes::from(vec![27, 91, 67]))?,
-                                    KeyCode::Up => sender.send(Bytes::from(vec![27, 91, 65]))?,
-                                    KeyCode::Down => sender.send(Bytes::from(vec![27, 91, 66]))?,
-                                    KeyCode::Home => sender.send(Bytes::from(vec![27, 91, 72]))?,
-                                    KeyCode::End => sender.send(Bytes::from(vec![27, 91, 70]))?,
-                                    KeyCode::PageUp => {
-                                        sender.send(Bytes::from(vec![27, 91, 53, 126]))?
-                                    }
-                                    KeyCode::PageDown => {
-                                        sender.send(Bytes::from(vec![27, 91, 54, 126]))?
-                                    }
-                                    KeyCode::Tab => sender.send(Bytes::from(vec![9]))?,
-                                    KeyCode::BackTab => {
-                                        sender.send(Bytes::from(vec![27, 91, 90]))?
-                                    }
-                                    KeyCode::Delete => {
-                                        sender.send(Bytes::from(vec![27, 91, 51, 126]))?
-                                    }
-                                    KeyCode::Insert => {
-                                        sender.send(Bytes::from(vec![27, 91, 50, 126]))?
-                                    }
-                                    KeyCode::Esc => {
-                                        sender.send(Bytes::from(vec![27]))?;
-                                    }
-                                    _ => {}
-                                }
+                                let _ = self.handle_terminal_key(key.code, key.modifiers, &sender);
                             }
                         }
                     }
@@ -235,6 +191,64 @@ impl App {
                     }
                     _ => {}
                 }
+            }
+        }
+    }
+
+    fn handle_popup_key(&mut self, key: KeyCode) -> Result<(), std::sync::mpsc::SendError<Bytes>> {
+        match key {
+            KeyCode::Enter => {
+                self.active_popup = None;
+                Ok(())
+            }
+            _ => Ok(()),
+        }
+    }
+
+    fn handle_terminal_key(
+        &mut self,
+        key: KeyCode,
+        modifiers: KeyModifiers,
+        sender: &Sender<Bytes>,
+    ) -> Result<(), std::sync::mpsc::SendError<Bytes>> {
+        if modifiers == KeyModifiers::CONTROL {
+            match key {
+                KeyCode::Char('c') => sender.send(Bytes::from(vec![3])),
+                KeyCode::Char('v') => sender.send(Bytes::from(vec![22])),
+                KeyCode::Char('d') => sender.send(Bytes::from(vec![4])),
+                KeyCode::Char('z') => sender.send(Bytes::from(vec![26])),
+                KeyCode::Char('l') => sender.send(Bytes::from(vec![12])),
+                KeyCode::Char('h') => {
+                    self.active_popup = Some(Popup::Help);
+                    Ok(())
+                }
+                _ => Ok(()),
+            }
+        } else {
+            match key {
+                KeyCode::Char(input) => sender.send(Bytes::from(input.to_string().into_bytes())),
+                KeyCode::Backspace => sender.send(Bytes::from(vec![8])),
+                KeyCode::Enter => {
+                    #[cfg(unix)]
+                    sender.send(Bytes::from(vec![b'\n']))?;
+                    #[cfg(windows)]
+                    sender.send(Bytes::from(vec![b'\r', b'\n']))?;
+                    Ok(())
+                }
+                KeyCode::Left => sender.send(Bytes::from(vec![27, 91, 68])),
+                KeyCode::Right => sender.send(Bytes::from(vec![27, 91, 67])),
+                KeyCode::Up => sender.send(Bytes::from(vec![27, 91, 65])),
+                KeyCode::Down => sender.send(Bytes::from(vec![27, 91, 66])),
+                KeyCode::Home => sender.send(Bytes::from(vec![27, 91, 72])),
+                KeyCode::End => sender.send(Bytes::from(vec![27, 91, 70])),
+                KeyCode::PageUp => sender.send(Bytes::from(vec![27, 91, 53, 126])),
+                KeyCode::PageDown => sender.send(Bytes::from(vec![27, 91, 54, 126])),
+                KeyCode::Tab => sender.send(Bytes::from(vec![9])),
+                KeyCode::BackTab => sender.send(Bytes::from(vec![27, 91, 90])),
+                KeyCode::Delete => sender.send(Bytes::from(vec![27, 91, 51, 126])),
+                KeyCode::Insert => sender.send(Bytes::from(vec![27, 91, 50, 126])),
+                KeyCode::Esc => sender.send(Bytes::from(vec![27])),
+                _ => Ok(()),
             }
         }
     }
