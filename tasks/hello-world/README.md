d328ee90eb2bb70c12dfe1cfe13010deb4c49e35
gohy907 <gohy2006@mail.ru>
2026-02-23 17:48:07 +0400

    chore: clean up scripts

--- tasks/hello-world/README.md
+++
@@ -1,6 +0,0 @@
-Чтобы загрузить репозиторий, сделайте следующее:
-
-```bash
-git clone git@github.com:git-trainer-tasks/hello-world src/repo
-tar czf src.tar.gz src
-```
--- tasks/hello-world/build.sh
+++ tasks/hello-world/build.sh
@@ -1,2 +1,4 @@
-git clone git@github.com:git-trainer-tasks/hello-world tasks/hello-world/src/repo
+# Вот так стоит добавлять репозиторий из организации git-trainer-tasks для создания образа задания.
+# В случае задания hello-world стартового репозитория не должно быть, поэтому тут ничего и не клонируется
+# git clone git@github.com:git-trainer-tasks/hello-world tasks/hello-world/src/repo 
 docker build -f tasks/hello-world/src/Dockerfile -t git-trainer:hello-world .
