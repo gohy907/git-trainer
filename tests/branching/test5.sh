7b0c5a813ab179354559222bb26123e5d6c4637b
gohy907 <gohy2006@mail.ru>
2026-03-15 21:09:35 +0400

    feat: add more tests

--- tests/branching/test5.sh
+++ tests/branching/test5.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-cd "$HOME"
+cd "$HOME/counting-sort"
 
-cd counting-sort &>/dev/null
-
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-MAIN_TIP=$(git rev-parse main)
-MERGE_BASE=$(git merge-base HEAD main)
-
-if [ "$MAIN_TIP" = "$MERGE_BASE" ]; then
-    echo "Ветка $CURRENT_BRANCH создана от main"
+if [ "$(git show-branch --merge-base print_vector remotes/origin/counting_sort)" = "790d2418057a3b0f0af1bd67d8704d1da92c179f" ]; then
+	
+    echo "5. Ветка print_vector создана от main."
     exit 0
 else
-    echo "Ветка $CURRENT_BRANCH НЕ создана от main (общий предок: $MERGE_BASE)"
+    echo "5. Убедитесь, что ветка print_vector создана от main."
     exit 1
 fi
