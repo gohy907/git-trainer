ff2367ea77bc97e42754fb3f49d11c0179ef7eea
gohy907 <gohy2006@mail.ru>
2026-03-19 20:14:56 +0400

    fix: shurum-burum

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ find tasks/ -name "build.sh" -exec ./{} \;
 cargo build --release -p tui 
 sudo mkdir -p /var/lib/git-trainer/ 
 sudo cp -r tests migrations schema.sql /var/lib/git-trainer/
+sudo cp target/release/tui /var/lib/git-trainer/git-trainer
